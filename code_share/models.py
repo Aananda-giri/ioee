8d44668d572a0e8425c9b9efc29df99ffd284071
hustleer <amaryesh123456@gmail.com>
2021-12-18 20:29:09 +0545

    copy btn debug

--- code_share/models.py
+++ code_share/models.py
@@ -7,38 +7,6 @@ from django.contrib.postgres.fields import ArrayField
 # settings.configure()
 
 
-class Post(models.Model):
-    post = models.CharField(max_length=50, default=None, null=True)
-    body = models.CharField(max_length=500, default=None)
-    loves = models.IntegerField(default=0)
-    email = models.EmailField(default=None,)
-
-    pub_date = models.DateTimeField('date published', default=timezone.now)
-
-    def __str__(self):
-        return('post: {}'.format(self.post))
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
-
-class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['created_on']
-
-    def __str__(self):
-        return 'Comment {} by {}'.format(self.body, self.name)
-
-
 class Code(models.Model):
     #id = models.AutoField(primary_key=True)
     id = models.UUIDField(
@@ -60,10 +28,65 @@ class Code(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return 'Comment {} by {}'.format(self.code, self.author)
+        return 'Code {} by {}'.format(self.code, self.author)
 
+class Branch(models.Model):
+    id = models.AutoField(primary_key=True)
+    originalCode = models.ForeignKey(Code, on_delete=models.CASCADE)
+    #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    code = models.TextField()
+    author = models.CharField(max_length=80, default='')
+    email = models.EmailField(default='')
+    created_on = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, default='')
+    branch_name = models.CharField(max_length=20, default='')
+    tags = ArrayField(models.CharField(
+        max_length=150, blank=True), default=None, null=True)
 
-# from code_share.models import Code
+    # False if the email is valid (i.e. could not send the mail)
+    valid_email = models.BooleanField(default=True)
+    # To hide the code from home page and search
+    hide_code = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on'] #to order based on created_date
+
+    def __str__(self):
+        return 'Code {} by {}'.format(self.code, self.author)
+###########################################
+# Unused models
+###########################################
+class Post(models.Model):
+    post = models.CharField(max_length=50, default=None, null=True)
+    body = models.CharField(max_length=500, default=None)
+    loves = models.IntegerField(default=0)
+    email = models.EmailField(default=None,)
+
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+
+    def __str__(self):
+        return('post: {}'.format(self.post))
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
+
+# from code_share.models import Code, CodeBranch
 # Code.objects.using('fuse_attend').all()[0].valid_email
 # Code.objects.using('fuse_attend').all()[0].hide_code
 
--- code_share/views.py
+++ code_share/views.py
@@ -1,4 +1,4 @@
-from .models import Code
+from .models import Code, Branch
 from .forms import CodeForm
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
@@ -9,42 +9,10 @@ from django.conf import settings
 from django.core import serializers
 
 
-# source: https://djangocentral.com/creating-codes-system-with-django/
-def post_detail(request, slug):
-    template_name = 'post_detail.html'
-    post = get_object_or_404(Post, slug=slug)
-    codes = post.codes.filter(active=True)
-    new_code = None
-    # code posted
-    if request.method == 'POST':
-        code_form = CodeForm(data=request.POST)
-        if code_form.is_valid():
-
-            # Create code object but don't save to database yet
-            new_code = code_form.save(commit=False)
-            # Assign the current post to the code
-            new_code.post = post
-            # Save the code to the database
-            new_code.save()
-    else:
-        code_form = CodeForm()
-
-    return render(request, template_name, {'post': post,
-                                           'codes': codes,
-                                           'new_code': new_code,
-                                           'code_form': code_form})
-
-
-def format_email_message_body(uuid):
-    message = "Here is your code: https://ioee.herokuapp.com/code/{}/\n\n".format(
-        str(uuid))
-    #message += "anyone who has this link has right to delete it?"
-    #message += "You can delete this code by verifying this email"
-    return(message)
 
 
 def home(request):
-    template_name = 'post_detail.html'
+    template_name = 'code_share/home.html'
     #post = get_object_or_404(Post, slug=slug)
 
     new_code = None
@@ -75,26 +43,114 @@ def home(request):
     data = serializers.serialize('json', codes)
     #data = serializers.serialize('json', {'codes': codes, 'new_code': new_code, 'code_form': code_form} )
 
-    return render(request, 'code_share/index.html', {'data': data, 'new_code': new_code, 'code_form': code_form})
+    return render(request, 'code_share/home.html', {'data': data, 'new_code': new_code, 'code_form': code_form})
 
 
-'''
-def Code(request):
-    if request.is_ajax and request.method == "POST":
-        # get the nick name from the client side.
-        #post = request.POST.get("username", None);
-        code = request.POST.get("code", None);
-        author = request.POST.get("author", None);
-        
-    if not str(code).strip()=='':
-        Codes.objects.using('fuse_attend').create(code=str(code), author = str(author))
-        return HttpResponse('Successfully codeed')
-    
+
+
+
+def format_email_message_body(uuid):
+    message = "Here is your code: https://ioee.herokuapp.com/code/{}/\n\n".format(
+        str(uuid))
+    #message += "anyone who has this link has right to delete it?"
+    #message += "You can delete this code by verifying this email"
+    return(message)
+
+def send_mail_please(recipient, subject="uuid", message='hello World'):
+    # create a variable to keep track of the form
+    #messageSent = False
+    #recipient = 'aanandaprashadgiri@gmail.com'
+    #subject = "Sending an email with Django"
+    #message = cd['message']
+
+    # send the email to the recipent
+    send_mail(subject, message,
+              settings.DEFAULT_FROM_EMAIL, recipient)
+
+    # set the variable initially created to True
+    messageSent = True
+
+
+def code_by_uuid(request, uuid):
+    code = Code.objects.using('fuse_attend').get(id=uuid)
+    # return render(request, 'code_share/individual_code.html', {'code':code})
+    code_form = CodeForm()
+    #codes = Code.objects.using('fuse_attend').all().order_by('-created_on')
+    data = serializers.serialize('json', [code])
+    #data = serializers.serialize('json', {'codes': codes, 'new_code': new_code, 'code_form': code_form} )
+
+    return render(request, 'code_share/home.html', {'data': data, 'code_form': code_form})
+
+def editCode(request):
+    template_name = 'home_experimental.html'
+    #post = get_object_or_404(Post, slug=slug)
+
+    new_code = None
+    # code posted
+    if request.method == 'POST':
+            #code_form = CodeForm(data=request.POST)
+            #if code_form.is_valid():
+            code = request.POST.get("code", None)
+            title = request.POST.get("title", None)
+            author = request.POST.get("author", None)
+            email = request.POST.get("email", None)
+            tags = request.POST.get("tags", None).split(' ')
+            print(code, title, email)
+            # Create code object but don't save to database yet
+            branch = Branch.objects.using('fuse_attend').create(originalCode=code, code=code, email=email, title=title, tags=tags, author=author)
+            branch.save() # Saves the branch
+            #new_code = Code.objects.using('fuse_attend').create(
+                #code=code, email=email, title=title, tags=tags, author=author)
+            # Assign the current post to the code
+            
+            
+            
+            print('\n\n {} \n\n'.format(str(new_code.id)))
+            if email.strip() != '':
+                send_mail_please(recipient=[
+                                 email], subject="code", message=format_email_message_body(str(new_code.id)))
+    # else:
+    code_form = CodeForm()
+    codes = Code.objects.using('fuse_attend').all().order_by('-created_on')
+    data = serializers.serialize('json', codes)
+    #data = serializers.serialize('json', {'codes': codes, 'new_code': new_code, 'code_form': code_form} )
+
+    return render(request, template_name, {'data': data, 'new_code': new_code, 'code_form': code_form})
+
+###########################################
+# Unused views
+###########################################
+
+# source: https://djangocentral.com/creating-codes-system-with-django/
+def post_detail(request, slug):
+    template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    codes = post.codes.filter(active=True)
+    new_code = None
+    # code posted
+    if request.method == 'POST':
+        code_form = CodeForm(data=request.POST)
+        if code_form.is_valid():
+
+            # Create code object but don't save to database yet
+            new_code = code_form.save(commit=False)
+            # Assign the current post to the code
+            new_code.post = post
+            # Save the code to the database
+            new_code.save()
     else:
-        return HttpResponse('code is empty')
+        code_form = CodeForm()
 
-'''
+    return render(request, template_name, {'post': post,
+                                           'codes': codes,
+                                           'new_code': new_code,
+                                           'code_form': code_form})
 
+
+
+def ssl_cert(request):
+    #requires heroku hobby dynos
+    return HttpResponse('3ufveaLc9JnIgj9y5-f9jrhb8Vgz2fSAFTbKguBTMYk.s1y7_YhjSk90w1NT8OBQ3LfEfRjn-eWv8R014Xl2Cco')
 
 def sendMail(request):
 
@@ -129,31 +185,19 @@ def sendMail(request):
 
     })
 
+'''
+def Code(request):
+    if request.is_ajax and request.method == "POST":
+        # get the nick name from the client side.
+        #post = request.POST.get("username", None);
+        code = request.POST.get("code", None);
+        author = request.POST.get("author", None);
+        
+    if not str(code).strip()=='':
+        Codes.objects.using('fuse_attend').create(code=str(code), author = str(author))
+        return HttpResponse('Successfully codeed')
+    
+    else:
+        return HttpResponse('code is empty')
 
-def send_mail_please(recipient, subject="uuid", message='hello World'):
-    # create a variable to keep track of the form
-    #messageSent = False
-    #recipient = 'aanandaprashadgiri@gmail.com'
-    #subject = "Sending an email with Django"
-    #message = cd['message']
-
-    # send the email to the recipent
-    send_mail(subject, message,
-              settings.DEFAULT_FROM_EMAIL, recipient)
-
-    # set the variable initially created to True
-    messageSent = True
-
-
-def code_by_uuid(request, uuid):
-    code = Code.objects.using('fuse_attend').get(id=uuid)
-    # return render(request, 'code_share/individual_code.html', {'code':code})
-    code_form = CodeForm()
-    #codes = Code.objects.using('fuse_attend').all().order_by('-created_on')
-    data = serializers.serialize('json', [code])
-    #data = serializers.serialize('json', {'codes': codes, 'new_code': new_code, 'code_form': code_form} )
-
-    return render(request, 'code_share/index.html', {'data': data, 'code_form': code_form})
-
-def ssl_cert(request):
-    return HttpResponse('3ufveaLc9JnIgj9y5-f9jrhb8Vgz2fSAFTbKguBTMYk.s1y7_YhjSk90w1NT8OBQ3LfEfRjn-eWv8R014Xl2Cco')
+'''
