cb65797b691694bcddc03bd6ebfff118cf08bb50
hustleer <amaryesh123456@gmail.com>
2021-12-20 19:32:44 +0545

    requirements.py dbug

--- ioee/settings.py
+++ ioee/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'person.apps.PersonConfig',
     'api.apps.ApiConfig',
     'code_share.apps.CodeShareConfig',
-    #'fontawesome-free',
     'nepali_datetime_field',
 ]
+#'fontawesome-free',
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
