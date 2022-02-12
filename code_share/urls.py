dd7a82834de3e6017978e1bf746142a88bfd19dd
hustleer <amaryesh123456@gmail.com>
2022-02-12 13:36:43 +0545

    update

--- code_share/urls.py
+++ code_share/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.snippet_page, name='home'),
+    #path('', views.home, name='home'),
+    path('', views.snippet_page, name='snippet_first'),
     path('<int:page>/', views.snippet_page, name="get_snippets"),
     path('code/', views.home, name='toy_post'),
     path('edit/<str:parent_id>/', views.edit_code, name='edit_code_by_parent_id'),
