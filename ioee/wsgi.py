79643d6ffd3bf55c5525bb23d9377ce7b6700cb3
aanandaprashadgiri@gmail.com <aanandaprashadgiri@gmail.com>
2023-01-30 07:45:56 +0545

    req

--- ioee/wsgi.py
+++ ioee/wsgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+# sys.path.append("../settings.py")
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ioee.settings')
