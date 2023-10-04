310172583f1d8f7caed11f1d2dc85c30af9a5464
aananda-giri <aanandaprashadgiri@gmail.com>
2023-10-04 11:48:26 +0545

    debug

--- pdf_engine/views.py
+++ pdf_engine/views.py
@@ -19,7 +19,7 @@ def search(request):
         try:
             search_result = requests.get('http://ec2-18-206-249-251.compute-1.amazonaws.com:5000/search', params={'query': query})
             search_result.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx)
-            # search_result = search_result.json()
+            search_result = search_result.json()
         except Exception as err:
             print(f'error: {err}')
             search_result = requests.get('https://strangehacksoldquestionsolution.aanandagiri.repl.co/search', params={'query': query}).json()
