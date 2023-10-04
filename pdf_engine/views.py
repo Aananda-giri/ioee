f5a1734a6d77ed982bf8960de00f6dd523912e61
aananda-giri <aanandaprashadgiri@gmail.com>
2023-10-04 11:31:44 +0545

    update api

--- pdf_engine/views.py
+++ pdf_engine/views.py
@@ -5,16 +5,26 @@ import requests
 # Create your views here.
 def search(request):
     # get the search query from get request
-    query = request.GET.get('search', 'physics')
+    query = request.GET.get('search', '')
     
     print(f'\n\n query: {query} \n\n {request}')
     
-    # get the search results from the api
-    # with open('test_search.json','r') as f:
-    #     search_result = json.load(f)
-    search_result = requests.get('http://ec2-3-88-84-244.compute-1.amazonaws.com:5000/search', params={'query': query}).json()
-    # search_result = requests.get('https://strangehacksoldquestionsolution.aanandagiri.repl.co/search', params={'query': query}).json()
+    if query == '':
+        # get the search results from the api
+        with open('sample_search_result.json','r') as f:
+            search_result = json.load(f)
+        query = 'physics'
+    else:
+        # get results from second api if first api fails
+        try:
+            search_result = requests.get('http://ec2-18-206-249-251.compute-1.amazonaws.com:5000/search', params={'query': query})
+            search_result.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx)
+            search_result = search_result.json()
+        except requests.exceptions.HTTPError as err:
+            print(f'error: {err}')
+            search_result = requests.get('https://strangehacksoldquestionsolution.aanandagiri.repl.co/search', params={'query': query}).json()
+        
     return render(request, 'pdf_engine/search.html', {'search_result': search_result})
 
-# requests.get('http://ec2-3-88-84-244.compute-1.amazonaws.com:5000/search', params={'query': 'physics'}).json()['result'][0]
+# requests.get(http://ec2-18-206-249-251.compute-1.amazonaws.com:5000/search', params={'query': 'physics'}).json()['result'][0]
 # requests.get('https://strangehacksoldquestionsolution.aanandagiri.repl.co/search', params={'query': 'physics'}).json()['result'][0]
