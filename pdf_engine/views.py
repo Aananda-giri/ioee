365b5d1ae2f178139ebf2f2193fad1ee52924305
aananda-giri <aanandaprashadgiri@gmail.com>
2023-10-21 10:34:04 +0545

    changing order of pdf-search result fetch

--- pdf_engine/views.py
+++ pdf_engine/views.py
@@ -17,12 +17,19 @@ def search(request):
     else:
         # get results from second api if first api fails
         try:
+            print('try')
+            search_result = requests.get('https://strangehacksoldquestionsolution.aanandagiri.repl.co/search', params={'query': query})
+            search_result.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx)
+            search_result = search_result.json()
+            
+            print(search_result)
+        except Exception as err:
+            print('except')
+            print(f'error: {err}')
             search_result = requests.get('http://ec2-18-206-249-251.compute-1.amazonaws.com:5000/search', params={'query': query})
             search_result.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx)
             search_result = search_result.json()
-        except Exception as err:
-            print(f'error: {err}')
-            search_result = requests.get('https://strangehacksoldquestionsolution.aanandagiri.repl.co/search', params={'query': query}).json()
+            
         
     return render(request, 'pdf_engine/search.html', {'search_result': search_result})
 
